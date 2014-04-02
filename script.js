623ff8671f921ef2e113909a213f03b23bae7a04
Alex Naser <c.a.naser@gmail.com>
2014-04-01 21:55:03 -0400

    mp4s playing from local files
    success!

---
+++ script.js
@@ -0,0 +1,28 @@
+function el(id) {
+  return document.getElementById(id);
+}
+
+var Video = function(fileList) {
+   this.file = fileList[0];
+   this.type = this.file.type; 
+   this.name = this.file.name;
+   this.createVideoURL();
+ }
+
+Video.prototype.createVideoURL = function(){
+  var URL = window.URL || window.webkitURL,
+      file = this.file;
+
+   this.fileURL = URL.createObjectURL(file);
+ };
+
+function onInputChange(ev) {
+  ev.preventDefault();
+
+  var fileList = ev.currentTarget.files;
+  var video = new Video(fileList); 
+  
+  el('video').src = video.fileURL;
+}
+
+el("input").addEventListener("change", onInputChange);
